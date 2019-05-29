5626760232680e9366ea6438b275fe7ca71e7d79
cXiaof <310257509@qq.com>
2019-05-29 18:09:30 +0800

    first commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+class ConditionSpelling extends Component {
+    render() {
+        return <div>ConditionSpelling</div>
+    }
+}
+
+export default ConditionSpelling
---
+++ stories/index.stories.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import { storiesOf } from '@storybook/react'
+
+import ConditionSpelling from '../src/index'
+
+storiesOf('ConditionSpelling', module).add('default', () => (
+    <ConditionSpelling />
+))
